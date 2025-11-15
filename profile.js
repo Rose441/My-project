67c64c05322f04328d1e19dec94a1dc9ab37a0a4
Flower <florencengz06@gmail.com>
2025-11-15 11:30:04 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -5,8 +5,8 @@ const links = [
       "http://wa.me/+2349110981508",
       "http://www.linkedin.com/in/florence-onwudebelu-123456789",
       "http://www.facebook.com/profile.php?id=100079048759051",
-      "http://www.instagram.com/fiora_dev"
-      
+      "http://www.instagram.com/fiora_dev",
+      "https://x.com/florence_n37924?t=Ow6MH5VTKOwVoxc2qH9-pQ&s=09"
       
 ];
   for (let i = 0; i < gitEl.length; i++) {
@@ -14,5 +14,6 @@ const links = [
           window.open(links[i], "_blank" );
     });
   } 
+
 
 
